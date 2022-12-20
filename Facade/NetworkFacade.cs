31f99a35b1ae19b1cbff4df7d6653e68669e1475
Hinyi <mrmck16@gmail.com>
2022-12-20 16:27:28 +0100

    Added NetworkFacade.cs

---
+++ Facade/NetworkFacade.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Facade;
+
+public class NetworkFacade
+{
+    private Packet _packet;
+    private Socket _socket;
+    private Transmission _transmission;
+
+    public NetworkFacade(string ip, string protocol, int port)
+    {
+        this._packet = new Packet(ip);
+        this._socket = new Socket(port);
+        this._transmission = new Transmission(protocol);
+    }
+
+    public void BuiltNetworklayer()
+    {
+        _packet.BuiltPacket();
+        _socket.SocketBuilt();
+    }
+
+    public void SendPackerOverNetwork()
+    {
+        BuiltNetworklayer();
+        _transmission.SendTransmission();
+    }
+}
